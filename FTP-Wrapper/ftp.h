fbe7ccbb4e7a714cdfd0cbd702926e099bcbb6e2
elielouis <40942423+elielouis@users.noreply.github.com>
2020-03-04 14:44:46 +0200

    Add files via upload

---
+++ FTP-Wrapper/ftp.h
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include <WinInet.h>
+#include <iostream>
+#pragma comment (lib, "wininet.lib")
+
+class FTP
+{
+	public:
+		FTP();
+		~FTP();
+		bool connect(LPCWSTR lpszServerName, LPCWSTR lpszUser, LPCWSTR lpszPassword);
+		bool upload(LPCWSTR lpszLocation, LPCWSTR lpszRemFile);
+		bool download(LPCWSTR lpszRemName, LPCWSTR lpszLocation);
+		bool mkdir(LPCWSTR lpszDirectory);
+		bool cd(LPCWSTR lpszDirectory);
+		bool remove(LPCWSTR lpszFileName);
+		bool rename(LPCWSTR lpszOldFileName, LPCWSTR lpszNewFileName);
+		bool rmdir(LPCWSTR lpszDirectory);
+		int getfilesize(LPCWSTR lpszFileName);
+		HINTERNET gethandle();
+		std::string getcd();
+
+
+	private:
+		bool m_connected;
+		HINTERNET m_hInternet;
+		HINTERNET m_hFtpSession;
+};
